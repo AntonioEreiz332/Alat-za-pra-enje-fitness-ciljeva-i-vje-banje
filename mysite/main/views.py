f9aa217b1e5bba2fe1c0c440650d6393aead5685
AntonioEreiz332 <58472078+AntonioEreiz332@users.noreply.github.com>
2024-12-18 23:36:22 +0100

    Add files via upload

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.views.generic import ListView, DetailView
+from .models import *
 
 # Create your views here.
 from django.contrib.auth.models import User
@@ -26,3 +28,69 @@ def admin_dashboard(request):
 def user_dashboard(request):
     return render(request, 'user_dashboard.html')
 
+class VjezbaListView(ListView):
+    model = Vjezba
+    template_name = 'vjezba_list.html'
+    context_object_name = 'vjezbe'
+
+    def get_queryset(self):
+
+        #Filtriranje prema korisniku i pretraživanje prema nazivu vježbe.
+        queryset = super().get_queryset()
+        korisnik = self.request.GET.get('korisnik')  # Filtriranje prema korisniku
+        search_query = self.request.GET.get('search')  # Pretraživanje prema nazivu vježbe
+
+        if korisnik:
+            queryset = queryset.filter(korisnik__korisnik__username=korisnik)
+        if search_query:
+            queryset = queryset.filter(naziv_vjezbe__icontains=search_query)
+
+        return queryset
+
+class FitnessCiljListView(ListView):
+    model = FitnessCilj
+    template_name = 'fitness_cilj_list.html'
+    context_object_name = 'ciljevi'
+
+    def get_queryset(self):
+        
+        #Filtriranje prema korisniku i pretraživanje prema nazivu cilja.
+        queryset = super().get_queryset()
+        korisnik = self.request.GET.get('korisnik')
+        search_query = self.request.GET.get('search')
+
+        if korisnik:
+            queryset = queryset.filter(korisnik__korisnik__username=korisnik)
+        if search_query:
+            queryset = queryset.filter(naziv_cilja__icontains=search_query)
+
+        return queryset
+    
+class KorisnickiProfilListView(ListView):
+    model = KorisnickiProfil
+    template_name = 'korisnickiprofil_list.html'
+    context_object_name = 'profili'
+
+    def get_queryset(self):
+
+        #Pretraživanje profila prema korisničkom imenu.        
+        queryset = super().get_queryset()
+        search_query = self.request.GET.get('search')
+        if search_query:
+            queryset = queryset.filter(korisnik__username__icontains=search_query)
+        return queryset
+
+class VjezbaDetailView(DetailView):
+    model = Vjezba
+    template_name = 'vjezba_detail.html'
+    context_object_name = 'vjezba'
+
+class FitnessCiljDetailView(DetailView):
+    model = FitnessCilj
+    template_name = 'fitness_cilj_detail.html'
+    context_object_name = 'cilj'
+
+class KorisnickiProfilDetailView(DetailView):
+    model = KorisnickiProfil
+    template_name = 'korisnickiprofil_detail.html'
+    context_object_name = 'profil'
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path
-from main import views
+from main import views 
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +27,14 @@ urlpatterns = [
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('user-dashboard/', views.user_dashboard, name='user_dashboard'),
     path('', views.home, name='home'),  # Ovo je URL za 'home'
+   
+    # ListView
+    path('vjezbe/', views.VjezbaListView.as_view(), name='vjezba_list'),
+    path('ciljevi/', views.FitnessCiljListView.as_view(), name='fitnesscilj_list'),
+    path('profili/', views.KorisnickiProfilListView.as_view(), name='korisnickiprofil_list'),
+    
+    # DetailView
+    path('vjezba/<int:pk>/', views.VjezbaDetailView.as_view(), name='vjezba_detail'),
+    path('cilj/<int:pk>/', views.FitnessCiljDetailView.as_view(), name='fitnesscilj_detail'),
+     path('profili/<int:pk>/', views.KorisnickiProfilDetailView.as_view(), name='korisnickiprofil_detail'),
 ]
